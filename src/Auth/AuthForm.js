3a4cd7f87fcbc16b8da42e0d4f07868161c302e7
Gomathi Manoharan <gomathiecebe@gmail.com>
2022-03-10 22:05:44 +0530

    login form UI fixing

--- src/Auth/AuthForm.js
+++ src/Auth/AuthForm.js
@@ -6,29 +6,31 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
   return (
-    <section className='auth'>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className='control'>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className='control'>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className='actions'>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className='toggle'
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
+    <div className = 'login_pop-up'>
+      <section className='auth'>
+        <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+        <form>
+          <div className='control'>
+            <label htmlFor='email'>Your Email</label>
+            <input type='email' id='email' required />
+          </div>
+          <div className='control'>
+            <label htmlFor='password'>Your Password</label>
+            <input type='password' id='password' required />
+          </div>
+          <div className='actions'>
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+            <button
+              type='button'
+              className='toggle'
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? 'Create new account' : 'Login with existing account'}
+            </button>
+          </div>
+        </form>
+      </section>
+    </div>
   );
 };
 
